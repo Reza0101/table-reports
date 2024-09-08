e3028c7ae92ea283dc37028b6969c47903d5bdca
reza <rezasiahi8@gmail.com>
2024-09-08 15:44:29 +0330

    feat: add function Formats a number

--- src/helpers/function.ts
+++ src/helpers/function.ts
@@ -2,8 +2,8 @@
 // If the number is negative, it will prepend a negative sign
 // For example, -5 will be formatted as "-%5"
 
-export default function formatNegativeNumbers(number: number) {
+export default function formatNegativeNumbers(number: number){
         if (number < 0) {
             return `-%${Math.abs(number)}`;
         } else return `%${number}`
-}
+};
